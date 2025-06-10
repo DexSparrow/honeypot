8c3f7bafcdc4fd855681b39dda347ae3601d9f9e
DexSparrow_on_VM <denhilarion147@gmail.com>
2025-06-10 05:16:47 +0000

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "Be root :)"
     exit 1
 fi
-sudo mkdir -p /var/www/honeypot
-sudo cp -r ./frontend/* /var/www/honeypot/
+sudo mkdir -p /var/www/honeypotsite
+sudo cp -r ./frontend/* /var/www/honeypotsite/
+sudo cp honeypotsite /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/honeypotsite /etc/nginx/sites-enabled/
